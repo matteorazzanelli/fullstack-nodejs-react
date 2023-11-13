ba4398f6f43b91004c4cecb7e8cb2e3abb887957
matteo <matteo.razzanelli89@gmail.com>
2023-11-13 07:59:16 +0100

    feat: add a user model bridge function to check if a email is already in use

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -7,10 +7,19 @@ class UserModel extends GeneralModel {
     console.log('user model')
   }
 
-  // async insert(content, table){
-  //   // check if a user with the same email already exist in DB
-  //   super.insert(content, table);
-  // }
+  async insert(content, table){
+    // check if a user with the same email already exist
+    const key = Object.keys(content).filter(key => content[key]===content.email);
+    const result = await super.select({[key]:content.email}, table);
+    if(result.rows.length === 0){
+      console.log('aggiungi')
+      return await super.insert(content, table)
+    }
+    else{
+      console.log('esiste già')
+      return  {...result, rows: {...result.rows, affectedRows:[]}}
+    }
+  }
   
 }
 
