bcfb3252349b574d8c69d05569d34359068a118b
matteo <matteo.razzanelli89@gmail.com>
2023-11-20 07:55:52 +0100

    fix: correct visualization of the username in the home page and force redirection in case the user is not logged in

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 import RobotWrapper from '../components/robot/RobotWrapper';
@@ -7,7 +7,23 @@ export default function Home() {
 
   const navigate = useNavigate();
 
-  const [user, setUser] = useState(localStorage.getItem('user-info'));
+  const [user, setUser] = useState(undefined);
+
+  useEffect(()=>{
+    const userName = (localStorage.getItem('user-info'));
+    if(userName===null){
+      alert('Session expired')
+      navigate('/')
+    }
+    else{
+      setUser(userName
+        .replaceAll('"', '')
+        .split("@")[0]
+        .toUpperCase()
+      );
+    }
+    // eslint-disable-next-line
+  },[])
 
   const logout = () => {
     localStorage.removeItem("user-info");
@@ -16,7 +32,7 @@ export default function Home() {
 
   return (
     <>
-      <div>Hi, {user}</div>
+      {user && <div className='welcome'>Hi, {user}</div>}
       <RobotWrapper/>
       <div className='submit-btn' onClick={logout}>
         Logout
