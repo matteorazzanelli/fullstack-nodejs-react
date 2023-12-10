88ee939b881f329499a0fe04e3bd03e32b934b1c
matteo <matteo.razzanelli89@gmail.com>
2023-12-10 19:17:03 +0100

    log

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,5 @@ app.use(routes);
 
 // start listen to port 5000
 require('dotenv').config({ path: '.env' });
+console.log(process.env.MYSQLPORT)
 const server = app.listen(process.env.MYSQLPORT, '0.0.0.0');
