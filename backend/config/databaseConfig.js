254a2a4acf63a8f98a99260f2d871d788d45404c
matteo <matteo.razzanelli89@gmail.com>
2023-12-10 18:55:26 +0100

    refactor: deploy

--- backend/config/databaseConfig.js
+++ backend/config/databaseConfig.js
@@ -2,11 +2,19 @@ require('dotenv').config({ path: '.env' });
 
 const mysql = require('mysql2');
 
+// const connection = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PSSWORD,
+//   database: process.env.DB_DATABASE
+// })
+
 const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PSSWORD,
-  database: process.env.DB_NAME
-})
+  host: MYSQLHOST,
+  user: MYSQLUSER,
+  password: MYSQLPASSWORD,
+  database: MYSQLDATABASE,
+  port: MYSQLPORT
+});
 
 module.exports = connection;
--- backend/server.js
+++ backend/server.js
@@ -7,4 +7,5 @@ const routes = require('./routes');
 app.use(routes);
 
 // start listen to port 5000
-const server = app.listen(5000, '127.0.0.1');
+require('dotenv').config({ path: '.env' });
+const server = app.listen(MYSQLPORT, 'https://cheery-mermaid-5c4862.netlify.app/');
