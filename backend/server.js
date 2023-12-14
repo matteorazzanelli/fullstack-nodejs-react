aab75859c63c905b3c0f2bca52cb8ea75aeff498
matteo <matteo.razzanelli89@gmail.com>
2023-12-14 07:57:07 +0100

    refactor: some configurations

--- backend/server.js
+++ backend/server.js
@@ -8,5 +8,5 @@ app.use(routes);
 
 // start listen to port 5000
 require('dotenv').config({ path: '.env' });
-console.log(process.env.MYSQLPORT)
-const server = app.listen(process.env.MYSQLPORT, '0.0.0.0');
+const port = process.env.MYSQLPORT || 5000;
+const server = app.listen(port, '0.0.0.0');
