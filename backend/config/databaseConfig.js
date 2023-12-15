494997d4b0eadf11c12897ffc8e3187b32af0b9c
matteo <matteo.razzanelli89@gmail.com>
2023-12-15 07:55:31 +0100

    refactor

--- backend/config/databaseConfig.js
+++ backend/config/databaseConfig.js
@@ -2,18 +2,11 @@ require('dotenv').config({ path: '.env' });
 
 const mysql = require('mysql2');
 
-// const connection = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PSSWORD,
-//   database: process.env.DB_DATABASE
-// })
-
 const connection = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST || process.env.DB_HOST,
+  user: process.env.MYSQLUSER || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PSSWORD,
+  database: process.env.MYSQLDATABASE || process.env.DB_DATABASE,
   port: process.env.MYSQLPORT
 });
 
